e30ad75d72cf77df9f5d44b8d4d112046c251191
IgorGomesLeiteDosAnjos <igor.gomes@liderautomacao.com>
2024-10-28 16:15:20 -0300

    feat(refatory): login

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './core/App'
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ToastContainer } from 'react-toastify'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ToastContainer />
     </QueryClientProvider>
   </StrictMode>,
 )
